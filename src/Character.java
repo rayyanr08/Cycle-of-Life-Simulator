3ae13d1a9a83556ef3c1de455cc1285a323b2a0d
BT_2S14_25 <@>
2024-11-12 10:57:46 -0500

    added multiple methods for character class and created new activities class. also worked on some of the logic in main class. Need to fix the print problem in main class and figure out how to structure activities class.

--- src/Character.java
+++ src/Character.java
@@ -2,12 +2,11 @@ public class Character {
     private String name;
     private String nationality;
     private String ethnicity;
+    public boolean isAlive;
 
 
-    public Character (String name, String nationality, String ethnicity){
+    public Character (String name){
         this.name = name;
-        this.nationality = nationality;
-        this.ethnicity = ethnicity;
     }
 
 
@@ -22,7 +21,22 @@ public class Character {
     public void generateEthnicity(){
         String[] genEthnicity = {"American, Spanish, Indian"};
         int num = (int)(Math.random() * genEthnicity.length);
+        ethnicity = genEthnicity[num];
 
+    }
+
+    public String generateFamily(){
+        String [] motherName = {"Maria, Anna, Karen"};
+        String [] fatherName = {"John, Matthew, Raymond"};
+        String regex = " ";
+        String[] lastNameArray = name.split(regex);    // last name matches user's last name
+        String lastName = lastNameArray[1];
+        int num = (int)(Math.random() * motherName.length);
+        int numF = (int)(Math.random() * fatherName.length);
+        String mother = motherName[num] + lastName;
+        String father = fatherName[numF] + lastName;
+
+        return "Your mother is " + mother + "\n" + "Your father is " + father;
 
     }
 
@@ -32,7 +46,10 @@ public class Character {
         return "Your name is " + name + "\n" + "You live in " + nationality + "\n" + "Your parents are " + ethnicity;
     }
 
-
+    public String generateReputation(){
+        int num = (int)(Math.random() * 50 + 50);
+        return "Your reputation is at " + num + "%";
+    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the cycle of life: ");
+        System.out.println("Welcome to the cycle of life! ");
+        System.out.println("This game simulates your life in an imaginary world.");
+        System.out.println("You can interact with different people and perform different activities while trying to maintain a reputation");
+        System.out.println("Let's get started!");
+
+
+        // Asking user for name
+        Scanner name = new Scanner(System.in);
+        System.out.println("Please enter your FULL name: ");
+        String rName = name.nextLine();
+        Character person = new Character(rName);
+        person.generateEthnicity();
+        person.generateNationality();
+        person.generateFamily();
+        person.getDescription();
+
 
     }
 }
