449d38adf2df498b75509b69c39ca61168c8f9db
rayyanr08 <rayyanrahman26@gmail.com>
2024-11-19 19:56:00 -0500

    Update School.java
    completed method for school class and implemented it into runner file

--- src/School.java
+++ src/School.java
@@ -4,13 +4,13 @@ public class School {
     public School() {
     }
 
-    public String quiz1Math(int number) {
+    public String quiz1Math(Integer[] userAns) {
         String[] mathQuestions = {"How many thousands are in a million?", "Which value is the smallest prime number?"};
         Integer[] mathAnswers = {1000, 2};
         boolean isTrue = true;
         if (isTrue) {
             for (int i = 0; i <= mathQuestions.length; i++) {
-                if (number != mathAnswers[i]) {
+                if (userAns[i] != mathAnswers[i]) {
                     return "Incorrect! YOU FAIL";
                 } else {
                     return "Correct! YOU PASS";
