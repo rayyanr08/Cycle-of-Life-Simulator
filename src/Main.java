3a7bca60c6a33d6d471eec8d9cbb09f8a2b37702
rayyanr08 <rayyanrahman26@gmail.com>
2024-11-20 23:36:08 -0500

    Update Main.java
    made code look presentable in main class and debugged and created many new methods for school and activities class

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the cycle of life! ");
+        System.out.println("---------------------------------");
         System.out.println("This game simulates your life in an imaginary world.");
         System.out.println("You perform different tasks as a child and as you get older, you will unlock more activities to do");
         System.out.println("With each activity you perform, you spend one year of your life");
         System.out.println("Let's get started!");
+        System.out.println("---------------------------------");
 
 
         // Asking user for name
@@ -15,19 +17,23 @@ public class Main {
         System.out.println("Please enter your FULL name: ");
         String rName = name.nextLine();
 
+        System.out.println("---------------------------------");
         Character person = new Character(rName);
         person.generateEthnicity();
         person.generateNationality();
 
         System.out.println(person.generateFamily());
         System.out.println(person.getDescription());
+        System.out.println("---------------------------------");
 
 
         Activities user = new Activities();
+
         // childhood
             System.out.println("As a child, you can interact with your parents or play by yourself");
             Scanner childhood = new Scanner(System.in);
             for (int i = 0; i < 5; i++) {
+                System.out.println("---------------------------------");
                 System.out.println("Type 'mom' to spend time with your mother");
                 System.out.println("Type 'dad' to spend time with your father");
                 System.out.println("Type 'alone' to play with toys by yourself");
@@ -43,36 +49,81 @@ public class Main {
         System.out.println("Getting one question wrong means you FAIL the test");
         System.out.println("Doing bad in subjects will affect your ability to get a job in the future");
 
+
+        //MATH QUIZ
         System.out.println("Math Quiz:");
+        System.out.println("---------------------------------");
         School studentQuiz = new School();
         Scanner userInput = new Scanner(System.in);
-        String[] questions = {"How many thousands are in a million?",  "Which value is the smallest prime number?"};
 
-        ArrayList userAnswers = new ArrayList();
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println(questions[i]);
+
+        String[] questions = {"How many thousands are in a million?", "Which value is the smallest prime number?"};
+
+
+        ArrayList<Integer> userAnswers = new ArrayList<>();
+        for (String question : questions) {
+            System.out.println(question);
             userAnswers.add(userInput.nextInt());
-
         }
 
         String result = studentQuiz.quiz1Math(userAnswers);
         System.out.println(result);
+        user.addAge();
+        System.out.println("You are now " + user.getAge() + " years old");
+        System.out.println("---------------------------------");
+
+        // SCIENCE QUIZ
+        System.out.println("Science Quiz:");
+        System.out.println("---------------------------------");
+        School studentQuiz2 = new School();
+        Scanner userInput2 = new Scanner(System.in);
+
+        String[] questionsScience = {"How many bones do sharks have in their body?", "How many teeth does a human adult have?"};
+
+        ArrayList<Integer> userAnswersScience = new ArrayList<>();
+        for (String question : questionsScience) {  // Loop through the correct questions array
+            System.out.println(question);
+            userAnswersScience.add(userInput2.nextInt());  // Add answers to the correct list
+        }
+
+        String resultScience = studentQuiz.quiz1Science(userAnswers);
+        System.out.println(result);
+        user.addAge();
+        System.out.println("You are now " + user.getAge() + " years old");
+        System.out.println("---------------------------------");
+
+        // ENGLISH QUIZ
+        System.out.println("English Quiz:");
+        System.out.println("---------------------------------");
+        School studentQuiz3 = new School();
+        Scanner userInput3 = new Scanner(System.in);
+
+        String[] questionsEnglish = {"Which year romeo and juliet published in?", "How many lines are in a sonnet?"};
 
 
+        ArrayList<Integer> userAnswersEnglish = new ArrayList<>();
+        for (String question : questionsEnglish) {
+            System.out.println(question);
+            userAnswers.add(userInput.nextInt());
+        }
+
+        String resultEnglish = studentQuiz.quiz1Science(userAnswers);
+        System.out.println(result);
+        user.addAge();
+        System.out.println("You are now " + user.getAge() + " years old");
+        System.out.println("---------------------------------");
 
 
-            // teen years
+        // teen years
             if(user.getAge() == 10){
                 System.out.println("You are now allowed to meet new people and make friends!");
             }
             Scanner meetFriend = new Scanner(System.in);
-        System.out.println("Do you want to meet a new person?");
-
             for (int i = user.getAge(); i<=16; i++)
             {
-                System.out.println("Do you want to meet new people?");
+                System.out.println("Do you want to meet a new person?");
                 String ifMeet = meetFriend.nextLine();
-                System.out.println(user.meetNewPeople());
+                System.out.println(user.meetNewPeople(ifMeet));
 
             }
 
@@ -82,13 +133,24 @@ public class Main {
         System.out.println(user.getFriend(numFriends));
 
 
-// job
-        System.out.println("You became an adult, so now you have to choose a job. Based on your quiz results you can choose the following jobs");
-
 
 
         // activities until death
         System.out.println("You can choose from the following activities. All of these activities are dangerous and there is a chance that you will die.");
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for their choice of risky activity
+        System.out.println("Enter a risky activity you'd like to attempt:");
+        System.out.println("Type 1 to Climb the empire state building");
+        System.out.println("Type 2 to explore an abandoned cave");
+        System.out.println("Type 3 to sit down all day");
+        int userChoice = Integer.parseInt(scanner.nextLine());
+
+        String resultrisky = user.riskyActivity(userChoice);
+        System.out.println(resultrisky);
+        user.addAge();
+        System.out.println("Age: " + user.getAge());
+
 
 
 
